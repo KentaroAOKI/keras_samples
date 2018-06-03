e13e7e57409c870008fd08a51862f70bd354811d
Kentaro Aoki <kentaro.aoki@gmail.com>
2018-06-03 22:06:00 +0900

    add reuters sample code

--- reuters.py
+++ reuters.py
@@ -7,7 +7,7 @@ except ImportError:
 # Use Keras
 from keras import models
 from keras import layers
-from keras.datasets import imdb
+from keras.datasets import reuters
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,26 +18,27 @@ def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.
+    print(results)
     return results
 
-# Get IMDb data from Keras datasets and choose 10000 words with high frequency.
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+# Get Reuters data from Keras datasets and choose 10000 words with high frequency.
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 if will_print_data:
-    print(max([max(sequence) for sequence in train_data]))
-    word_index = imdb.get_word_index()
+    word_index = reuters.get_word_index()
     reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
     decode_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
-    print(decode_review)
 
 # Get maximum number of words to create input dimension.
 max_words = max([max(sequence) for sequence in train_data]) + 1
+# Get maximum number of labels to create output dimension.
+max_labels = max(train_labels) + 1
 
 # vectorize data for train
 x_train = vectorize_sequences(train_data, max_words)
-y_train = np.asarray(train_labels).astype('float32')
+y_train = vectorize_sequences(train_labels, max_labels)
 # vectorize data for test
 x_test = vectorize_sequences(test_data, max_words)
-y_test = np.asarray(test_labels).astype('float32')
+y_test = vectorize_sequences(test_labels, max_labels)
 
 # Split the x_train into train data and validation data
 len_val = int(len(x_train) * 0.4)
@@ -48,13 +49,12 @@ partial_y_train = y_train[len_val:]
 
 # Create model
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(max_words,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-
+model.add(layers.Dense(int(max_labels * 1.5), activation='relu', input_shape=(max_words,)))
+model.add(layers.Dense(int(max_labels * 1.5), activation='relu'))
+model.add(layers.Dense(max_labels, activation='softmax'))
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 # Fit
-history = model.fit(partial_x_train, partial_y_train, epochs=4, batch_size=512, validation_data=(x_val, y_val))
+history = model.fit(partial_x_train, partial_y_train, epochs=8, batch_size=512, validation_data=(x_val, y_val))
 
 # Print results
 result = model.evaluate(x_test, y_test)
